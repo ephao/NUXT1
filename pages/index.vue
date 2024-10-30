5422741f3a2677abc65149a6f1fe29e3f36fefc2
ephao <gzhdong@foxmail.com>
2024-10-30 06:05:07 -0700

    no css

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,23 @@
 <template>
-    <div class="w-full px-4 min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-pink-50 via-blue-50 to-purple-50">
-        <h1 class="text-4xl font-bold mb-4">Hello World</h1>
-        <p class="text-gray-500 mb-6">我们正在开发一系列有趣的工具</p>
-        
-        <UButton 
-            to="https://google.com" 
-            target="_blank" 
-            variant="outline"
-            class="px-8 hover:bg-gray-100"
-        >立即订阅</UButton>
+    <div class="w-full flex flex-col items-center justify-center min-h-screen bg-[url('/bg.jpg')] bg-cover bg-fixed ">   
+        <div><span>waitlist</span></div>
+        <div><h1>coming soon</h1></div>
+        <div>
+            <h2 class="text-4xl font-bold mb-4">Cow AI</h2>
+            <p class="text-gray-500 mb-6">A new tool for Livestock Farming</p>
+            <UButton 
+                to="https://google.com" 
+                target="_blank" 
+                variant="outline"
+                class="px-8 hover:bg-gray-100"
+            >Subscribe Now</UButton>
+        </div>
+        <div>
+            <a title="Follow X" href="https://x.com" >X </a>
+            <a title="Search in Google" href="https://google.com" >Google </a>
+        </div>  
     </div>        
 </template>
- 
 <script setup>
 
 </script>
